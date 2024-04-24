3fbc6ea7f21ea57fc161bb9aa6730d578820e0a9
botrkl <96987652+botrkl@users.noreply.github.com>
2024-04-25 01:47:16 +0300

    InjectBLLServices in Program.cs

--- TesttaskITExpert.Solution/TesttaskITExpert/Program.cs
+++ TesttaskITExpert.Solution/TesttaskITExpert/Program.cs
@@ -1,8 +1,10 @@
 using TesttaskITExpert.DAL.Extensions;
+using TesttaskITExpert.BLL.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.InjectDALServices(builder.Configuration);
+builder.Services.InjectBLLServices(builder.Configuration);
 
 var app = builder.Build();
 app.UseRouting();
