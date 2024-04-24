aadec26f5d44029168731a46ad1b30d5ecb5075c
botrkl <96987652+botrkl@users.noreply.github.com>
2024-04-25 01:44:51 +0300

    Implement BLLServiceCollectionExtension

---
+++ TesttaskITExpert.Solution/TesttaskITExpert.BLL/Extensions/BLLServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TesttaskITExpert.BLL.Services.Classes;
+using TesttaskITExpert.BLL.Services.Interfaces;
+
+namespace TesttaskITExpert.BLL.Extensions
+{
+    public static class BLLServiceCollectionExtension
+    {
+        public static void InjectBLLServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IFilmService, FilmService>();
+        }
+    }
+}
