b570eaba9fbae4988c5b775bee22f70cad7346d5
botrkl <96987652+botrkl@users.noreply.github.com>
2024-04-25 06:25:03 +0300

    Simple realisation create category view

---
+++ TesttaskITExpert.Solution/TesttaskITExpert/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesttaskITExpert.BLL.Models.AddModels;
+using TesttaskITExpert.BLL.Services.Interfaces;
+
+namespace TesttaskITExpert.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [Route("/addCategory")]
+        [HttpGet]
+        public async Task<IActionResult> CreateCategory()
+        {
+            var categories = await _categoryService.GetAllCategories();
+            ViewBag.Categories = categories;
+            return View();
+        }
+
+        [Route("/addCategory")]
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromForm] AddCategoryModel addCategoryModel)
+        {
+            await _categoryService.AddCategoryAsync(addCategoryModel);
+            return RedirectToAction("CreateCategory");
+        }
+    }
+}
