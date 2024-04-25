8bcc91349b76d279a88a31271431dee410771f1a
botrkl <96987652+botrkl@users.noreply.github.com>
2024-04-25 03:10:33 +0300

    inject AutoMapperes

--- TesttaskITExpert.Solution/TesttaskITExpert.BLL/Extensions/BLLServiceCollectionExtension.cs
+++ TesttaskITExpert.Solution/TesttaskITExpert.BLL/Extensions/BLLServiceCollectionExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TesttaskITExpert.BLL.Mapping;
 using TesttaskITExpert.BLL.Services.Classes;
 using TesttaskITExpert.BLL.Services.Interfaces;
 
@@ -9,6 +10,9 @@ namespace TesttaskITExpert.BLL.Extensions
     {
         public static void InjectBLLServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddAutoMapper(typeof(CategoryMapperProfile));
+            services.AddAutoMapper(typeof(FilmMapperProfile));
+
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IFilmService, FilmService>();
         }
