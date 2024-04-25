22b7adee6815e65b3f2dea356c30f15316841a0b
botrkl <96987652+botrkl@users.noreply.github.com>
2024-04-25 06:36:30 +0300

    Simple realisation create film view

---
+++ TesttaskITExpert.Solution/TesttaskITExpert/Controllers/FilmController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesttaskITExpert.BLL.Models.AddModels;
+using TesttaskITExpert.BLL.Services.Interfaces;
+
+namespace TesttaskITExpert.Controllers
+{
+    public class FilmController : Controller
+    {
+        private readonly IFilmService _filmService;
+        public FilmController(IFilmService filmService)
+        {
+            _filmService = filmService;
+        }
+        [Route("/addFilm")]
+        [HttpGet]
+        public IActionResult CreateFilm()
+        {
+            return View();
+        }
+        [Route("/addFilm")]
+        [HttpPost]
+        public async Task<IActionResult> CreateFilm([FromForm] AddFilmModel addFilmModel)
+        {
+            await _filmService.AddFilmAsync(addFilmModel);
+            return RedirectToAction("CreateFilm");
+        }
+    }
+}
